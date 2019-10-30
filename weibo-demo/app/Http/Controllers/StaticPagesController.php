048b92c8bee0d65dff361ccba8b66febe0767f89
songhe <songhe156@sina.com>
2019-10-30 22:31:54 +0800

    基础页面

---
+++ weibo-demo/app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesController extends Controller
+{
+    //首页
+    public function home() {
+        //return '主页';
+        return view('static_pages/home');
+    }
+
+    //帮助页
+    public function help() {
+        //return '帮助页';
+        return view('static_pages/help');
+    }
+
+    //关于页
+    public function about() {
+        //return '关于页';
+        return view('static_pages/about');
+    }
+}
