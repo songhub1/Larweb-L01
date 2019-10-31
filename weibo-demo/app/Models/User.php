6a7249238179996b07ec5c207214a8c880120eef
songhe <songhe156@sina.com>
2019-10-31 14:42:22 +0800

    move user model to models folder

--- weibo-demo/app/Models/User.php
+++ weibo-demo/app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -11,7 +11,14 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * 指明要进行数据库交互的数据库表名称
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * 过滤用户提交的字段，只有包含在该属性中的字段才能够被正常更新
      *
      * @var array
      */
@@ -20,7 +27,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for arrays.
+     * 当我们需要对用户密码或其它敏感信息在用户实例通过数组或 JSON 显示时进行隐藏.
      *
      * @var array
      */
